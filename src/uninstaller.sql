957f5d99e3a76d026d3a841a1c3ea6423abb4ccf
liu12 <8981024@qq.com>
2022-10-28 10:46:37 +0800

    删除 SQL 删表

--- src/uninstaller.sql
+++ src/uninstaller.sql
@@ -1,2 +1,7 @@
 DROP TABLE IF EXISTS `company_contact_message`;
 DROP TABLE IF EXISTS `company_team`;
+DROP TABLE IF EXISTS `company_feedback`;
+DROP TABLE IF EXISTS `company_partner`;
+DROP TABLE IF EXISTS `company_faq`;
+DROP TABLE IF EXISTS `company_announcement`;
+DROP TABLE IF EXISTS `company_link`;
